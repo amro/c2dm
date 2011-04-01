f68e5de01b5dcec4c9217254a8b7111d3d0aa235
Amro Mousa <amro@return7.com>
2011-03-31 20:59:33 -0400

    Add support for multiple key-value pairs in payload

--- lib/c2dm.rb
+++ lib/c2dm.rb
@@ -1,48 +1,70 @@
 require 'httparty'
 require 'cgi'
 
-module C2DM
-  class Push
-    include HTTParty
-    default_timeout 30
+class C2DM
+  include HTTParty
+  default_timeout 30
 
-    attr_accessor :timeout
+  attr_accessor :timeout
 
-    AUTH_URL = 'https://www.google.com/accounts/ClientLogin'
-    PUSH_URL = 'https://android.apis.google.com/c2dm/send'
+  AUTH_URL = 'https://www.google.com/accounts/ClientLogin'
+  PUSH_URL = 'https://android.apis.google.com/c2dm/send'
 
-    def initialize(username, password, source)
-      post_body = "accountType=HOSTED_OR_GOOGLE&Email=#{username}&Passwd=#{password}&service=ac2dm&source=#{source}"
-      params = {:body => post_body,
-                :headers => {'Content-type' => 'application/x-www-form-urlencoded', 
-                             'Content-length' => "#{post_body.length}"}}
+  def initialize(username, password, source = "MyCompany-MyAppName-1.0")
+    post_body = "accountType=HOSTED_OR_GOOGLE&Email=#{username}&Passwd=#{password}&service=ac2dm&source=#{source}"
+    params = {
+      :body => post_body, 
+      :headers => {
+        'Content-type' => 'application/x-www-form-urlencoded', 
+        'Content-length' => "#{post_body.length}"
+      }
+    }
 
-      response = Push.post(AUTH_URL, params)
-      
-      raise response if response["Error="]
-      
-      response_split = response.body.split("\n")
-      @auth_token = response_split[2].gsub("Auth=", "")
-    end
+    response = self.class.post(AUTH_URL, params)
 
-    def send_notification(registration_id, message)
-      post_body = "registration_id=#{registration_id}&collapse_key=foobar&data.message=#{CGI::escape(message)}"
-      params = {:body => post_body,
-                :headers => {'Authorization' => "GoogleLogin auth=#{@auth_token}"}}
+    raise response if response["Error="] #Raise error in case of auth failure
 
-      response = Push.post(PUSH_URL, params)
-      response
-    end
-
-    def self.send_notifications(username, password, source, notifications)
-      c2dm = Push.new(username, password, source)
-    
-      responses = []
-      notifications.each do |notification|
-        responses << {:body => c2dm.send_notification(notification[:registration_id], notification[:message]), :registration_id => notification[:registration_id]}
-      end
-      responses
-    end
-
+    response_split = response.body.split("\n")
+    @auth_token = response_split[2].gsub("Auth=", "")
   end
+
+  # {
+  #   :registration_id => "...",
+  #   :data => {
+  #     :some_message => "Hi!", 
+  #     :another_message => 7
+  #   }
+  #   :collapse_key => "optional collapse_key string"
+  # }
+  def send_notification(options)
+    post_body = []
+    options.delete(:data).each_pair do |k,v| 
+      post_body << "data.#{k}=#{CGI::escape(v.to_s)}"
+    end
+    
+    options.each_pair do |k,v| 
+      post_body << "#{k}=#{CGI::escape(v.to_s)}"
+    end
+    
+    post_body = post_body.join("&")
+    params = {
+      :body => post_body, 
+      :headers => {
+        'Authorization' => "GoogleLogin auth=#{@auth_token}",
+        'Content-type' => 'application/x-www-form-urlencoded',
+        'Content-length' => "#{post_body.length}"
+      }
+    }
+    self.class.post(PUSH_URL, params)
+  end
+
+  def self.send_notifications(username, password, source, notifications)
+    c2dm = C2DM.new(username, password, source)
+    responses = []
+    notifications.each do |notification|
+      responses << {:body => c2dm.send_notification(notification), :registration_id => notification[:registration_id]}
+    end
+    responses
+  end
+
 end
